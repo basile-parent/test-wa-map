c1138751537e29850443615cec72f716cad26b09
Basile <casou.acc@gmail.com>
2021-04-03 18:17:08 +0200

    Some tests

--- assets/floortileset.tsx
+++
@@ -1,9 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.5.0" name="floortileset" tilewidth="32" tileheight="32" spacing="2" margin="1" tilecount="64" columns="8">
- <image source="../assets/floortileset.png" width="272" height="272"/>
- <tile id="37">
-  <properties>
-   <property name="collides" type="bool" value="true"/>
-  </properties>
- </tile>
-</tileset>
