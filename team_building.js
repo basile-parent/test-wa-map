6a48c1ffbfa7467736efba51cc580001d0d78623
Basile <casou.acc@gmail.com>
2021-04-26 14:21:03 +0200

    Some new stuffs

--- team_building.js
+++ team_building.js
@@ -1,3 +1,13 @@
 WA.onEnterZone('warden', () => {
-  WA.sendChatMessage("Hello!", 'Mr Robot');
+  WA.sendChatMessage("Mot de passe ?", 'Gardien');
 })
+
+WA.onChatMessage((message => {
+  switch (message) {
+    case "toto":
+      WA.sendChatMessage("Titi", 'Gardien');
+      break;
+    default:
+      WA.sendChatMessage("Ce n'est pas ça", 'Gardien');
+  }
+}));
