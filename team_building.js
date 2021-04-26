1c4c0291907b113bded995f0f9bcd468a22ebb3f
Basile <casou.acc@gmail.com>
2021-04-26 17:37:53 +0200

    Some new stuffs

--- team_building.js
+++ team_building.js
@@ -1,9 +1,20 @@
+let userOnZone = [];
+
 WA.onEnterZone('warden', () => {
   WA.sendChatMessage("Mot de passe ?", 'Gardien');
+  userOnZone.push("warden");
   console.log("WA", WA);
+})
+WA.onLeaveZone('warden', () => {
+  userOnZone = userOnZone.filter(z => z !== "warden");
 })
 
 WA.onChatMessage((message => {
+  if (!userOnZone.includes("warden")) {
+    WA.sendChatMessage("Vous êtes trop loin, je ne vous entends pas !", 'Gardien');
+    return;
+  }
+
   switch (message) {
     case "toto":
       WA.sendChatMessage("Titi", 'Gardien');
